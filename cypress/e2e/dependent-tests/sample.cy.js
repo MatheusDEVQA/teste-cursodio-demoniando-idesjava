d6d5c280930b731554300829c657a618ea174e23
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2024-05-13 22:17:01 -0300

    Add scenarios dependets adn abstrations

--- cypress/e2e/dependent-tests/sample.cy.js
+++ cypress/e2e/dependent-tests/sample.cy.js
@@ -1,8 +1,6 @@
 describe('Dependent tests bad practice', () => {
   beforeEach(() => {
-    cy.visit('http://notes-serverless-app.com')
-
-    cy.get('.navbar-nav a:contains(Login)').click()
+    cy.visit('http://notes-serverless-app.com/login')
 
     cy.get('#email').type(Cypress.env('user_email'))
     cy.get('#password').type(Cypress.env('user_password'), { log: false })
@@ -11,28 +9,28 @@ describe('Dependent tests bad practice', () => {
     cy.contains('h1', 'Your Notes').should('be.visible')
   })
 
-  it('creates a note', () => {
+  it('CRUDs a note', () => {
     cy.contains('Create a new note').click()
 
     cy.get('#content').type('My note')
     cy.contains('Create').click()
 
-    cy.get('.list-group').should('contain', 'My note')
-  })
-
-  it('edits a note', () => {
-    cy.get('.list-group').contains('My note').click()
+    cy.get('.list-group')
+      .should('contain', 'My note')
+      .click()
+    
     cy.get('#content').type(' updated')
     cy.contains('Save').click()
 
     cy.get('.list-group').should('contain', 'My note updated')
     cy.get('.list-group:contains(My note updated)').should('be.visible')
-  })
+    .click()
 
-  it('deletes a note', () => {
-    cy.get('.list-group').contains('My note updated').click()
     cy.contains('Delete').click()
 
+    cy.get('.list-group a')
+    .its('length')
+      .should('be.at.least', 1)
     cy.get('.list-group:contains(My note updated)').should('not.exist')
   })
 })
